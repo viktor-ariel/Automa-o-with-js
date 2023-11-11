d7d97b42d50cc9b76d88211cf68925fd109878bd
Viktor Ariel <98894099+viktor-ariel@users.noreply.github.com>
2023-11-11 19:11:23 -0300

    Add files via upload

---
+++ automação-js-zumbi/main.js
@@ -0,0 +1,12 @@
+var welcome = "hello world";
+document.getElementById("welcome").append(welcome);
+
+var idade = prompt("Qual sua idade?")
+
+if(idade>=18){
+    alert("Pode ir no show");
+}else{
+    alert("Não pode ir no show");
+}
+
+// laço de repetição
